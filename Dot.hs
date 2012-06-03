39ae0bfdc4fa843122f61fe88b8342fe472e6cb4
Bill <billehs+github@gmail.com>
2012-06-03 19:46:02 -0400

    First commit, just some junk from an opengl tutorial

---
+++ Dot.hs
@@ -0,0 +1,15 @@
+module Cube where
+import Graphics.Rendering.OpenGL
+import Graphics.UI.GLUT
+ 
+vertify3 :: [(GLfloat,GLfloat,GLfloat)] -> IO ()
+vertify3 verts = sequence_ $ map (\(a,b,c) -> vertex $ Vertex3 a b c) verts 
+ 
+cube w = renderPrimitive Quads $ vertify3
+      [ ( w, w, w), ( w, w,-w), ( w,-w,-w), ( w,-w, w),
+        ( w, w, w), ( w, w,-w), (-w, w,-w), (-w, w, w),
+        ( w, w, w), ( w,-w, w), (-w,-w, w), (-w, w, w),
+        (-w, w, w), (-w, w,-w), (-w,-w,-w), (-w,-w, w),
+        ( w,-w, w), ( w,-w,-w), (-w,-w,-w), (-w,-w, w),
+        ( w, w,-w), ( w,-w,-w), (-w,-w,-w), (-w, w,-w) ]
+
