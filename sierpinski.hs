78888511166a598691994ad07d51ecb4782507f1
Bill <billehs+github@gmail.com>
2012-06-03 22:42:10 -0400

    Added some chaos game logic;  drawDot now takes a Point2d instead of glfloats

--- sierpinski.hs
+++ sierpinski.hs
@@ -8,6 +8,7 @@ main = do
   (progname,_) <- getArgsAndInitialize
   createWindow "Hello World"
   reshapeCallback $= Just reshape
+
   --angle <- newIORef (0.0::GLfloat)
   --delta <- newIORef (0.1::GLfloat)
   --position <- newIORef (0.0::GLfloat, 0.0)
@@ -16,30 +17,54 @@ main = do
   displayCallback $= (drawDot 0 0)
   mainLoop
 
-drawDot :: GLfloat -> GLfloat -> IO ()
-drawDot x y = do
-  clear [ColorBuffer]
-  color blue
-  renderPrimitive Points $ vertex $ Vertex2 x y
-  flush
 
 reshape s@(Size w h) = do 
   viewport $= (Position 0 0, s)
 
 
-display' angle position = do 
-  clear [ColorBuffer]
-  loadIdentity
-  (x,y) <- get position
-  translate $ Vector3 x y 0
-  preservingMatrix $ do 
-    a <- get angle
-    rotate a $ Vector3 0 0 (1::GLfloat)
-    scale 0.7 0.7 (0.7::GLfloat)
-  swapBuffers
 
 idle angle delta = do
   a <- get angle
   d <- get delta
   angle $=! (a+d) --parens needed for a bug in StateVar
   postRedisplay Nothing
+
+
+
+{--
+A step for a chaos game, given three points and a list of random numbers
+we will draw at the midpoint of two of the random points selected
+randomly
+--}
+data Point2d = Point2d { 
+               xcoord :: GLfloat,
+               ycoord :: GLfloat 
+               } deriving (Show)
+
+
+chaosStep :: Point2d -> Point2d -> Point2d -> Point2d -> [Int] -> IO ()
+chaosStep _ _ _ [] = return ()
+chaosStep startPt pt0 pt1 pt2 (n:ns) = do
+  --Randomly choose a second point
+  let rndPt = case n `mod` 3
+      0 -> pt0
+      1 -> pt1
+      2 -> pt2
+
+  --Move current position to halfway between the starting point and our random point
+  mdPt = midpoint startPt rndPt
+  drawDot mdPt
+
+  putStrLn $ (show startPt) ++ "  " ++ (show rndPoint) ++ "  " ++ (show mdPt) --TODO Debug
+
+--Given two points, find the mid point between them
+midpoint :: Point2d -> Point2d -> Point2d
+midpoint (Point2d x1 y1) (Point2d x2 y2) = Point2d ((x1 + x2) / 2) ((y1 + y2) / 2)
+
+drawDot :: Point2d -> IO ()
+drawDot (Point2d x y) = do
+  clear [ColorBuffer]
+  color blue
+  renderPrimitive Points $ vertex $ Vertex2 x y
+  flush
+
